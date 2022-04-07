8fbca7fc6cc24b81c7bf1a81ae52798faaa89218
Cortezx25x <98494724+Cortezx25x@users.noreply.github.com>
2022-04-07 14:27:59 -0600

    Add files via upload

--- src/main/java/com/tienda/WebConfig.java
+++ src/main/java/com/tienda/WebConfig.java
@@ -4,45 +4,45 @@
  */
 package com.tienda;
 
-
 import java.util.Locale;
+import org.springframework.cglib.transform.impl.AddInitTransformer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-/**
- *
- * @author USUARIO
- */
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
+
     @Bean
-    public LocaleResolver localeResolver(){
+    public LocaleResolver localeResolver() {
         var slr = new SessionLocaleResolver();
         slr.setDefaultLocale(new Locale("es"));
         return slr;
+
     }
-    
-     @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor(){
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
         var lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
         return lci;
+
     }
-    
+
     @Override
-     public void addInterceptors(InterceptorRegistry registro){
-         registro.addInterceptor(localeChangeInterceptor());
-     }
-    
-    
-    
-    
-    
-    
-    
+    public void addInterceptors(InterceptorRegistry registro) {
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+        
+ @Override
+    public void addViewControllers(ViewControllerRegistry registro){
+        registro.addViewController("/").setViewName("index");
+        registro.addViewController("/login");
+        registro.addViewController("/errores/403").setViewName("/errores/403");
+    }
 }
