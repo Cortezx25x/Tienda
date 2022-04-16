6b7024905615b5b7e0e0d5fccd4eabfde0c1ef72
Cortezx25x <98494724+Cortezx25x@users.noreply.github.com>
2022-04-16 17:16:13 -0600

    Add files via upload

--- src/main/java/com/tienda/controller/ClienteController.java
+++ src/main/java/com/tienda/controller/ClienteController.java
@@ -20,11 +20,17 @@ public class ClienteController {
     private ClienteService clienteService;
     
     
-    @GetMapping("/cliente/listado")
+ @GetMapping("/cliente/listado")
     public String inicio(Model model) {
+        var clientes=clienteService.getClientes();
         
+        var limiteTotal=0;
+        for (var c: clientes) {
+            limiteTotal+=c.credito.getLimite();
+        }
+        model.addAttribute("limiteTotal",limiteTotal);
+        model.addAttribute("totalClientes",clientes.size());
         
-       var clientes= clienteService.getClientes();
         model.addAttribute("clientes",clientes);
         return "/cliente/listado";
     }
